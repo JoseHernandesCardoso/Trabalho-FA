90ad385715336358031dcd0c57743f65c6517e03
José Hernandes <josehhernandescardoso@gmail.com>
2025-07-11 22:33:32 -0300

    Ajusta jogos e separa times
    Ajusta a lista de jogos para conseguir extrair os dados com mais facilidade.
    Além disso, separa os times com suas estatíscias corretas, obtidas ao longo de cada
    jogo, dentro de uma única lista chamada times.

--- main.py
+++ main.py
@@ -60,6 +60,33 @@
 # Os times serão agrupados em uma lista de Time
 
 import sys
+from dataclasses import dataclass
+
+@dataclass
+class Jogo():
+    '''
+    Representação do resultado de um jogo de futebol
+    *gols_anfitriao* e *gols_visitante* são iguais ou maiores que zero
+    '''
+    anfitriao: str
+    gols_anfitriao: int
+    visitante: str
+    gols_visitante: int
+
+@dataclass
+class Time():
+    '''
+    Representação das estatísticas de um time de futebol em um campeonato
+    Todos os valores int são maiores ou iguais a zero
+    '''
+    nome: str
+    vitorias: int
+    pontuacao: int
+    saldo_gols: int
+    gols_sofridos: int
+    jogos_anfitriao: int
+    pontos_anfitriao: int
+
 
 def main():
     if len(sys.argv) < 2:
@@ -69,9 +96,13 @@ def main():
         print('Muitos parâmetro. Informe apenas um nome de arquivo.')
         sys.exit(1)
     jogos = le_arquivo(sys.argv[1])
+    converte_jogos(jogos)
+    times = define_times(jogos)
+
     # TODO: solução da pergunta 1
     # TODO: solução da pergunta 2
     # TODO: solução da pergunta 3
+
 
 def le_arquivo(nome: str) -> list[str]:
     '''
@@ -89,6 +120,140 @@ def le_arquivo(nome: str) -> list[str]:
     except IOError as e:
         print(f'Erro na leitura do arquivo "{nome}": {e.errno} - {e.strerror}.');
         sys.exit(1)
+
+def converte_jogos(jogos: list[str]):
+    '''
+    Modifica *jogos* convertendo cada elemento para o tipo Jogo().
+
+    Exemplos:
+    >>> rodada1 = ['Palmeiras 2 Flamengo 1', 'Bota-Fogo 0 Santos 0']
+    >>> rodada2 = ['Santos 0 Palmeiras 3', 'Flamengo 2 Bota-Fogo 1']
+    >>> converte_jogos(rodada1)
+    >>> rodada1 # doctest: +NORMALIZE_WHITESPACE
+    [Jogo(anfitriao='Palmeiras', gols_anfitriao=2, visitante='Flamengo', gols_visitante=1), 
+    Jogo(anfitriao='Bota-Fogo', gols_anfitriao=0, visitante='Santos', gols_visitante=0)]
+
+    >>> converte_jogos(rodada2)
+    >>> rodada2 # doctest: +NORMALIZE_WHITESPACE
+    [Jogo(anfitriao='Santos', gols_anfitriao=0, visitante='Palmeiras', gols_visitante=3),
+    Jogo(anfitriao='Flamengo', gols_anfitriao=2, visitante='Bota-Fogo', gols_visitante=1)]
+    '''
+    for i in range(len(jogos)):
+        dados = separa_no_espaco(jogos[i])
+        jogos[i] = Jogo(anfitriao=dados[0], gols_anfitriao=int(dados[1]),
+                        visitante=dados[2], gols_visitante=int(dados[3]))
+
+def separa_no_espaco(string: str) -> list[str]:
+    '''
+    Separa a *string* em strings menores onde tiver espaço em branco (' ').
+    Retorna uma lista dessas strings menores na mesma ordem de palavas da *string*
+
+    Exemplos:
+    >>> separa_no_espaco('')
+    []
+    >>> separa_no_espaco('Python')
+    ['Python']
+    >>> separa_no_espaco('Hello World!')
+    ['Hello', 'World!']
+    >>> separa_no_espaco('Escreva uma string')
+    ['Escreva', 'uma', 'string']
+    '''
+    separado = []
+    palavra = ''
+    for i in range(len(string)):
+        if string[i] != ' ':
+            palavra = palavra + string[i]
+            if i == len(string) - 1:
+                separado.append(palavra)
+        else:
+            separado.append(palavra)
+            palavra = ''
+    return separado
+
+def define_times(jogos: list[Jogo]) -> list[Time]:
+    '''
+    Define os times participantes e seus desempenhos em uma série de *jogos*
+
+    Exemplos:
+    >>> jogos = [
+    ... Jogo(anfitriao='Palmeiras', gols_anfitriao=2, visitante='Flamengo', gols_visitante=1),
+    ... Jogo(anfitriao='Bota-Fogo', gols_anfitriao=0, visitante='Santos', gols_visitante=0),
+    ... Jogo(anfitriao='Santos', gols_anfitriao=0, visitante='Palmeiras', gols_visitante=3),
+    ... Jogo(anfitriao='Flamengo', gols_anfitriao=2, visitante='Bota-Fogo', gols_visitante=1)
+    ... ]
+    >>> define_times(jogos) # doctest: +NORMALIZE_WHITESPACE
+    [Time(nome='Palmeiras', vitorias=2, pontuacao=6, saldo_gols=4, gols_sofridos=1,
+    jogos_anfitriao=1, pontos_anfitriao=3),
+    Time(nome='Flamengo', vitorias=1, pontuacao=3, saldo_gols=0, gols_sofridos=3,
+    jogos_anfitriao=1, pontos_anfitriao=3),
+    Time(nome='Bota-Fogo', vitorias=0, pontuacao=1, saldo_gols=-1, gols_sofridos=2,
+    jogos_anfitriao=1, pontos_anfitriao=1),
+    Time(nome='Santos', vitorias=0, pontuacao=1, saldo_gols=-3, gols_sofridos=3,
+    jogos_anfitriao=1, pontos_anfitriao=0)]
+    '''
+    times: list[Time] = []
+    for jogo in jogos:
+        # Coletando estatisticas do jogo
+        vitorias_anf = vitorias_vis = 0
+        pontuacao_anf = pontuacao_vis = 0
+        if jogo.gols_anfitriao > jogo.gols_visitante:
+            vitorias_anf = 1
+            pontuacao_anf = 3
+        elif jogo.gols_anfitriao == jogo.gols_visitante:
+            pontuacao_anf = pontuacao_vis = 1
+        else:
+            vitorias_vis = 1
+            pontuacao_vis = 3
+        # Atualizando ou criando (caso não exista) time anfitrião
+        i = procura_indice_time(jogo.anfitriao, times)
+        if i != -1:
+            times[i].vitorias = times[i].vitorias + vitorias_anf
+            times[i].pontuacao = times[i].pontuacao + pontuacao_anf
+            times[i].saldo_gols = times[i].saldo_gols + jogo.gols_anfitriao - jogo.gols_visitante
+            times[i].gols_sofridos = times[i].gols_sofridos + jogo.gols_visitante
+            times[i].jogos_anfitriao = times[i].jogos_anfitriao + 1
+            times[i].pontos_anfitriao = times[i].pontos_anfitriao + pontuacao_anf
+        else:
+            times.append(Time(
+                nome= jogo.anfitriao,
+                vitorias= vitorias_anf,
+                pontuacao= pontuacao_anf,
+                saldo_gols= jogo.gols_anfitriao - jogo.gols_visitante,
+                gols_sofridos= jogo.gols_visitante,
+                jogos_anfitriao= 1,
+                pontos_anfitriao= pontuacao_anf))
+        # Atualizando ou criando (caso não exista) time visitante
+        i = procura_indice_time(jogo.visitante, times)
+        if i != -1:
+            times[i].vitorias = times[i].vitorias + vitorias_vis
+            times[i].pontuacao = times[i].pontuacao + pontuacao_vis
+            times[i].saldo_gols = times[i].saldo_gols + jogo.gols_visitante - jogo.gols_anfitriao
+            times[i].gols_sofridos = times[i].gols_sofridos + jogo.gols_anfitriao
+        else:
+            times.append(Time(
+                nome= jogo.visitante,
+                vitorias= vitorias_vis,
+                pontuacao= pontuacao_vis,
+                saldo_gols= jogo.gols_visitante - jogo.gols_anfitriao,
+                gols_sofridos= jogo.gols_anfitriao,
+                jogos_anfitriao= 0,
+                pontos_anfitriao= 0))    
+    return times
+
+def procura_indice_time(nome: str, times: list[Time]) -> int:
+    '''
+    Retorna o indice de um time dentro de *times* dado o seu *nome*
+
+    Retorna -1 se o time não estiver em *times*
+    '''
+    indice_time = -1
+    i = 0
+    while i < len(times) and indice_time == -1:
+        if times[i].nome == nome:
+            indice_time = i
+        i = i + 1
+    return indice_time
+
 
 if __name__ == '__main__':
     main()
